eba0e78f62eb21a088c0ef8d13fa578a5256ebf7
Satya1296 <asrithasatya12@gmail.com>
2025-11-07 06:57:10 +0530

    Add files via upload

---
+++ 10c.js
@@ -0,0 +1,6 @@
+const fs=require('fs');
+const content = "HTML , CSS , Javascript , Typescript , MongoDB , Express.js , React.js , Node.js";
+fs.writeFile('src.txt' , content, (err) => {
+	if(err) throw err;
+	console.log('src.txt created and content added successfully!');
+});
---
+++ 10e.js
@@ -0,0 +1,3 @@
+const calc=require('./calculator');
+console.log("Addition:", calc.add(5,3));
+console.log("Multiplication:",calc.multiply(5,3));
